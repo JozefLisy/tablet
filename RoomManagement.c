b8f1e5ccce90779a34a3ad70e4ad609eab379d68
jojo <jozef1996lisy@gmail.com>
2024-03-03 18:14:33 +0000

    19:15 3.3.2024

---
+++ RoomManagement.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 3
+
+typedef struct{
+	int ID;
+	int countClients;
+}ROOM;
+
+void *thread_funct(void arg){
+	
+	
+	
+	return NULL;
+}
+
+int main(void){
+	ROOM room;
+	room.ID` = 5;	
+	
+	return 0;
+}
