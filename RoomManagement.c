4ed1230839454541daf3d2953a360ae7584b388b
jojo <jozef1996lisy@gmail.com>
2024-03-09 13:12:47 +0000

    Allocating memory for structure rooms. v1.00

--- RoomManagement.c
+++ RoomManagement.c
@@ -18,30 +18,11 @@ typedef struct{
 	CLIENT *clients;
 }ROOM;
 
-//ROOM *rooms = NULL;
-int condition = 1;
-
-
-void *thread_funct(void *arg){
-	long t_ID = (long) arg;
-	
-	while(condition){
-		printf("Hello from thread %ld/\n", t_ID);
-		sleep(1);
-	}
-	
-	printf("Release thread. ID: %ld\n", t_ID);
-	pthread_exit NULL;
-}
-
 void InicializeRoomsAtNULL(ROOM  **room){
 	if(*room == NULL){
 		printf("FUNCTION InicializeRooms: structure is empty.\n");
 		
-		// change malloc at realloc
-		//if((*room = (ROOM*)malloc(sizeof(ROOM))) != NULL){
 		if((*room = (ROOM*)realloc(*room, sizeof(ROOM))) != NULL){
-			// segmentation fault!!!	
 			(*room)->ID_room 	= NULL;
 			(*room)->countClients	= NULL;
 			(*room)->s_nameRoom 	= NULL;
@@ -57,26 +38,18 @@ void InicializeRoomsAtNULL(ROOM  **room){
 int SetRoomValues(ROOM **room, int *ID_room, char *s_nameRoom){
 	if((*room)->ID_room == NULL){
 		printf("(%d)SUCCESSFUL: room is NULL. (SetRoomValue)\n", __LINE__);
-		printf("Address: %p\n", (void*)(*room));
 		
 		if(((*room)->ID_room = (int*)malloc(sizeof(int))) != NULL){
 			*((*room)->ID_room) = *ID_room;
-			printf("Malloc for ID_room.\t\tAddress:\t%p\n", (void *)(*room)->ID_room);
 			
 			if(((*room)->countClients = (int*)malloc(sizeof(int))) != NULL){
-				(*room)->countClients = 0;
-				printf("Malloc for countClients.\tAddress:\t%p\n", (void*)(*room)->countClients);
+				*(*room)->countClients = 0;
 				
 				// zisti ako je to s '\0' v prijatom retazci. ci treba mat strlen + 1 alebo sa prepise aj ukoncovaci znak
 				if(((*room)->s_nameRoom = (char*)malloc((strlen(s_nameRoom) + 1) * sizeof(char))) != NULL){
 					if((strcpy((*room)->s_nameRoom, s_nameRoom)) != NULL){
-						printf("Malloc for s_nameRoom.\t\tAddress:\t%p\n", (void*)(*room)->s_nameRoom);						
-
-						// tu sa zavola funkcia pre inicializaciu prveho klienta?
-						//free(ID_room);
-						//free(s_nameRoom);
 						
-						printf("Pararams:\n\tAddress:\t%p\n\tAddress:\t%p\n\tAddress:\t%p\n", (void*)(*room), (void*)ID_room, (void*)s_nameRoom);
+						// tu sa zavola funkcia pre inicializaciu prveho klienta?
 						return 1;
 						
 					} else{
@@ -94,9 +67,6 @@ int SetRoomValues(ROOM **room, int *ID_room, char *s_nameRoom){
 	} else{
 		printf("(%d)ERROR: room is not null. (SetRoomValues)\n",__LINE__);
 	}
-	
-	//free(ID_room);
-	//free(s_nameRoom);
 	return 0;
 }
 
@@ -126,44 +96,6 @@ int main(void){
 	
 	ROOM *rooms = NULL;
 	
-	/*for(t = 0; t < NUM_THREADS; t++){
-		rc = pthread_create(&threads[t], NULL, thread_funct, (void*)(intptr_t)t);
-		if(rc){
-			printf("ERROR: %d\n", rc);
-			exit (-1);
-		}
-	} 
-	
-	getc(stdin);
-
-	condition = 0;
-
-	int exiting = 5;
-	for( ;exiting > 0; exiting--){
-		printf("exiting %d!\n", exiting);
-		sleep(1);
-	}
-
-	for (t = 0; t < NUM_THREADS; t++){
-		pthread_join(threads[t], NULL);
-	}
-	printf("All threads have completed.\n");
-	
-	if(rooms == NULL){
-		printf("rooms is NULL\n");
-		InicializeRoomsAtNULL(&rooms);
-		
-		int id = 3;
-		int count = 0;
-		char buffer[] = "hranolky";
-		if((SetRoomValues(&rooms, &id, buffer)) == 1){
-			printf("(%d)SUCCESSFUL: Set room values was successfully.\n",__LINE__);
-		}
-	} else{
-		printf("rooms is not NULL\n");
-		return 1;
-	}
-	*/
 	InicializeRoomsAtNULL(&rooms);
 
 	int id = 3;
@@ -173,9 +105,7 @@ int main(void){
 		printf("(%d)SUCCESSFUL: Set room values was successfully.\n",__LINE__);
 		FreeRoom(&rooms);
 	}
-	//FreeRoom(&rooms);
-	//free(rooms);
-
+	
 	printf("Bey");
 	return 0;
 }
