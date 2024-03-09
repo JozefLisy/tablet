4f302af317303a8baec285ececc726597f09f3a0
jojo <jozef1996lisy@gmail.com>
2024-03-09 22:56:12 +0000

    change malloc at realloc

--- RoomManagement.c
+++ RoomManagement.c
@@ -20,24 +20,24 @@ typedef struct{
 
 void InicializeRoomsAtNULL(ROOM  **room){
 	if(*room == NULL){
-		printf("FUNCTION InicializeRooms: structure is empty.\n");
+		printf("(%4d)REPORT: InicializeRooms: structure is empty.\n",__LINE__);
 		
 		if((*room = (ROOM*)realloc(*room, sizeof(ROOM))) != NULL){
 			(*room)->ID_room 	= NULL;
 			(*room)->countClients	= NULL;
 			(*room)->s_nameRoom 	= NULL;
 			(*room)->clients	= NULL;
-			printf("(%d)SUCCESFUL: inicialize was successfully.\n",__LINE__);
+			printf("(%4d)SUCCESFUL: inicialize was successfully.\n",__LINE__);
 		} else{
-			printf("(%d)ERROR: error allocating memory. (*room)\n",__LINE__);
+			printf("(%4d)ERROR: error allocating memory. (*room)\n",__LINE__);
 		}
 	} else
-		printf("(%d)ERROR: accepted structure is not empty\n",__LINE__);
+		printf("(%4d)ERROR: accepted structure is not empty\n",__LINE__);
 }
 
 int SetRoomValues(ROOM **room, int *ID_room, char *s_nameRoom){
 	if((*room)->ID_room == NULL){
-		printf("(%d)SUCCESSFUL: room is NULL. (SetRoomValue)\n", __LINE__);
+		printf("(%4d)REPORT: room is NULL. (SetRoomValue)\n", __LINE__);
 		
 		if(((*room)->ID_room = (int*)malloc(sizeof(int))) != NULL){
 			*((*room)->ID_room) = *ID_room;
@@ -46,10 +46,13 @@ int SetRoomValues(ROOM **room, int *ID_room, char *s_nameRoom){
 				*(*room)->countClients = 0;
 				
 				// zisti ako je to s '\0' v prijatom retazci. ci treba mat strlen + 1 alebo sa prepise aj ukoncovaci znak
-				if(((*room)->s_nameRoom = (char*)malloc((strlen(s_nameRoom) + 1) * sizeof(char))) != NULL){
+				
+				if(((*room)->s_nameRoom = (char*)malloc((strlen(s_nameRoom) + 1) * sizeof(char))) != NULL){ // + 1?
 					if((strcpy((*room)->s_nameRoom, s_nameRoom)) != NULL){
-						
+					
 						// tu sa zavola funkcia pre inicializaciu prveho klienta?
+						printf("(%4d)SUCCESSFUL: successfuly allocating memory for structure room.\n",__LINE__);
+						printf("\tValues: ID_room > %d / countClients >%d / s_nameRoom > %s\n", *(*room)->ID_room, *(*room)->countClients, (*room)->s_nameRoom);
 						return 1;
 						
 					} else{
@@ -103,9 +106,10 @@ int main(void){
 
 	if((SetRoomValues(&rooms, &id, buffer)) == 1){
 		printf("(%d)SUCCESSFUL: Set room values was successfully.\n",__LINE__);
-		FreeRoom(&rooms);
+		//FreeRoom(&rooms);
 	}
 	
+	FreeRoom(&rooms);
 	printf("Bey");
 	return 0;
 }
